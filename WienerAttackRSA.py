044a63a2d95e9b85c418e1532f991c81c35924e9
Philipp Ensinger <philippensi2@gmail.com>
2019-11-24 20:06:27 +0100

    Corrected help-message

--- WienerAttackRSA.py
+++ WienerAttackRSA.py
@@ -8,12 +8,12 @@ def main(argv):
     try:
         opts, args = getopt.getopt(argv, "hn:e:")
     except getopt.GetoptError:
-        print 'wienerRSAcrack.py -n <12345> -e <12345>'
+        print 'WienerAttackRSA.py -n <12345> -e <12345>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
             print 'wienerRSAcrack.py -n <12345> -e <12345>'
-            print 'Example: wienerRSAcrack.py -n 90581 -e 17993'
+            print 'Example: WienerAttackRSA.py -n 90581 -e 17993'
             sys.exit()
         elif opt == '-n':
             n = int(arg)
